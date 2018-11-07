31712f093e375e7c4a8431bad1289b626c6cd1da
Gonzalo <gonzalo224455@gmail.com>
2018-11-07 18:53:40 +0100

    Primer commir

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,67 +1,21 @@
 package application;
 
-import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
-import javafx.util.Duration;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
 public class Controller {
-	@FXML
-	Pane pane1;
-	@FXML
-	Pane pane2;
-	@FXML
-	Pane pane3;
-	@FXML
-	Pane pane4;
+	  @FXML
+	    private MediaView mediaView;
 
-	public void initialize() {
-		pane1.setStyle("-fx-background-image: url('application/1.jpg')");
-		pane2.setStyle("-fx-background-image: url('application/2.jpg')");
-		pane3.setStyle("-fx-background-image: url('application/3.jpg')");
-		pane4.setStyle("-fx-background-image: url('application/4.jpg')");
-		transicion();
+	    public void initialize() {
+
+	        Media media1= new Media(getClass().getResource("media/video.mp4").toExternalForm());
+	        MediaPlayer player=new MediaPlayer(media1);
+	        mediaView.setMediaPlayer(player);
+	        player.setVolume(0.5);
+	        player.play();
+
+	    }
 	}
-
-	private void transicion() {
-		FadeTransition ft1 = new FadeTransition(Duration.seconds(5), pane4);
-		ft1.setFromValue(1);
-		ft1.setToValue(0);
-		ft1.play();
-		ft1.setOnFinished(event1 -> {
-			FadeTransition ft2 = new FadeTransition(Duration.seconds(3), pane3);
-			ft2.setFromValue(1);
-			ft2.setToValue(0);
-			ft2.play();
-			ft2.setOnFinished(event2 -> {
-				FadeTransition ft3 = new FadeTransition(Duration.seconds(3), pane2);
-				ft3.setFromValue(0);
-				ft3.setToValue(1);
-				ft3.play();
-				ft3.setOnFinished(event3 -> {
-					FadeTransition ft4 = new FadeTransition(Duration.seconds(3), pane1);
-					ft4.setFromValue(0);
-					ft4.setToValue(1);
-					ft4.play();
-					ft4.setOnFinished(event4 -> {
-						FadeTransition ft5 = new FadeTransition(Duration.seconds(3), pane3);
-						ft5.setFromValue(0);
-						ft5.setToValue(1);
-						ft5.play();
-						ft5.setOnFinished(event5 -> {
-							FadeTransition ft6 = new FadeTransition(Duration.seconds(3), pane4);
-							ft6.setFromValue(0);
-							ft6.setToValue(1);
-							ft6.play();
-							ft6.setOnFinished(event6 -> {
-
-								transicion();
-							});
-
-						});
-					});
-				});
-			});
-		});
-	}
-}
